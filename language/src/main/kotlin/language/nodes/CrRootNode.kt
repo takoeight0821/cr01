a8796fb743e7dcc4e4ef7de77e4259c8bbd1f661
Yuya Kono <takohati0821@gmail.com>
2019-11-20 16:44:11 +0900

    キャプチャした環境のコピーを関数に分離

--- language/src/main/kotlin/language/nodes/CrRootNode.kt
+++ language/src/main/kotlin/language/nodes/CrRootNode.kt
@@ -1,9 +1,12 @@
 package language.nodes
 
+import com.oracle.truffle.api.dsl.NodeChild
+import com.oracle.truffle.api.dsl.NodeField
 import com.oracle.truffle.api.frame.*
 import com.oracle.truffle.api.nodes.RootNode
 import language.CrLanguage
 import language.nodes.expr.ExprNode
+import language.nodes.stmt.StmtNode
 
 class CrRootNode constructor(
     language: CrLanguage?,
@@ -12,47 +15,55 @@ class CrRootNode constructor(
     private val capturedEnv: MaterializedFrame? = null
 ) : RootNode(language, frameDescriptor) {
     override fun execute(frame: VirtualFrame): Any {
-        // TODO: extract to Node object
-        capturedEnv?.frameDescriptor?.slots?.forEach { slot: FrameSlot ->
-            when (capturedEnv.frameDescriptor.getFrameSlotKind(slot)!!) {
-                FrameSlotKind.Int -> {
-                    val value = FrameUtil.getIntSafe(capturedEnv, slot)
-                    frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Int)
-                    frame.setInt(slot, value)
+        capture(capturedEnv, frame)
+        return bodyNode.executeGeneric(frame)
+    }
+
+    companion object {
+        private fun capture(
+            capturedEnv: MaterializedFrame?,
+            frame: VirtualFrame
+        ) {
+            capturedEnv?.frameDescriptor?.slots?.forEach { slot: FrameSlot ->
+                when (capturedEnv.frameDescriptor.getFrameSlotKind(slot)!!) {
+                    FrameSlotKind.Int -> {
+                        val value = FrameUtil.getIntSafe(capturedEnv, slot)
+                        frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Int)
+                        frame.setInt(slot, value)
+                    }
+                    FrameSlotKind.Long -> {
+                        val value = FrameUtil.getLongSafe(capturedEnv, slot)
+                        frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Long)
+                        frame.setLong(slot, value)
+                    }
+                    FrameSlotKind.Boolean -> {
+                        val value = FrameUtil.getBooleanSafe(capturedEnv, slot)
+                        frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Boolean)
+                        frame.setBoolean(slot, value)
+                    }
+                    FrameSlotKind.Byte -> {
+                        val value = FrameUtil.getByteSafe(capturedEnv, slot)
+                        frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Byte)
+                        frame.setByte(slot, value)
+                    }
+                    FrameSlotKind.Double -> {
+                        val value = FrameUtil.getDoubleSafe(capturedEnv, slot)
+                        frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Double)
+                        frame.setDouble(slot, value)
+                    }
+                    FrameSlotKind.Float -> {
+                        val value = FrameUtil.getFloatSafe(capturedEnv, slot)
+                        frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Float)
+                        frame.setFloat(slot, value)
+                    }
+                    FrameSlotKind.Object -> {
+                        val value = FrameUtil.getObjectSafe(capturedEnv, slot)
+                        frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Object)
+                        frame.setObject(slot, value)
+                    }
+                    FrameSlotKind.Illegal -> Unit
                 }
-                FrameSlotKind.Long -> {
-                    val value = FrameUtil.getLongSafe(capturedEnv, slot)
-                    frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Long)
-                    frame.setLong(slot, value)
-                }
-                FrameSlotKind.Boolean -> {
-                    val value = FrameUtil.getBooleanSafe(capturedEnv, slot)
-                    frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Boolean)
-                    frame.setBoolean(slot, value)
-                }
-                FrameSlotKind.Byte -> {
-                    val value = FrameUtil.getByteSafe(capturedEnv, slot)
-                    frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Byte)
-                    frame.setByte(slot, value)
-                }
-                FrameSlotKind.Double -> {
-                    val value = FrameUtil.getDoubleSafe(capturedEnv, slot)
-                    frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Double)
-                    frame.setDouble(slot, value)
-                }
-                FrameSlotKind.Float -> {
-                    val value = FrameUtil.getFloatSafe(capturedEnv, slot)
-                    frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Float)
-                    frame.setFloat(slot, value)
-                }
-                FrameSlotKind.Object -> {
-                    val value = FrameUtil.getObjectSafe(capturedEnv, slot)
-                    frame.frameDescriptor.setFrameSlotKind(slot, FrameSlotKind.Object)
-                    frame.setObject(slot, value)
-                }
-                FrameSlotKind.Illegal -> Unit
             }
         }
-        return bodyNode.executeGeneric(frame)
     }
 }
