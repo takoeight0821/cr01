dd12273fdd5ae19235216fad2995d2084a806e86
Yuya Kono <takohati0821@gmail.com>
2019-11-20 17:33:13 +0900

    format code

--- language/src/main/kotlin/language/runtime/CrContext.kt
+++ language/src/main/kotlin/language/runtime/CrContext.kt
@@ -6,7 +6,7 @@ import com.oracle.truffle.api.TruffleLanguage
 import com.oracle.truffle.api.interop.TruffleObject
 import com.oracle.truffle.api.nodes.NodeInfo
 import java.io.PrintWriter
-import java.util.HashMap
+import java.util.*
 
 class CrContext(env: TruffleLanguage.Env) {
     private val functions: MutableMap<String, CrFunction> = HashMap()
--- language/src/test/java/language/LibraryTest.java
+++ language/src/test/java/language/LibraryTest.java
@@ -4,10 +4,10 @@
 package language;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
+    @Test
+    public void testSomeLibraryMethod() {
         // Library classUnderTest = new Library();
         // assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
